c678ec06ef6ccf4f4f00caa044f5412e95da24c8
Jepsiko <bibouda512@hotmail.com>
2019-11-26 10:39:23 +0100

    No inheritance for now

--- src/AbstractSyntaxTree.java
+++ src/AbstractSyntaxTree.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AbstractSyntaxTree {
-    Symbol label;
+    private Symbol label;
     private List<AbstractSyntaxTree> children;
 
     /**
--- src/ParseTree.java
+++ src/ParseTree.java
@@ -13,7 +13,8 @@ import java.util.ArrayList;
  * @author Léo Exibard, Sarah Winter
  */
 
-public class ParseTree extends AbstractSyntaxTree {
+public class ParseTree {
+    private Symbol label;
     private List<ParseTree> children;
 
     /**
@@ -32,8 +33,93 @@ public class ParseTree extends AbstractSyntaxTree {
      * @param children Its children
      */
     public ParseTree(Symbol label, List<ParseTree> children) {
-        super(label);
+        this.label = label;
         this.children = children;
+    }
+
+    /**
+     * Writes the tree as LaTeX code
+     */
+    public String toLaTexTree() {
+        StringBuilder treeTeX = new StringBuilder();
+        treeTeX.append("[");
+        treeTeX.append("{").append(label.getValue()).append("}");
+        treeTeX.append(" ");
+
+        for (ParseTree child : children) {
+            if (child != null)
+                treeTeX.append(child.toLaTexTree());
+        }
+        treeTeX.append("]");
+        return treeTeX.toString();
+    }
+
+    /**
+     * Writes the tree as TikZ code. TikZ is a language to specify drawings in LaTeX
+     * files.
+     */
+    public String toTikZ() {
+        StringBuilder treeTikZ = new StringBuilder();
+        treeTikZ.append("node {");
+        treeTikZ.append(label.getValue());
+        treeTikZ.append("}\n");
+        for (ParseTree child : children) {
+            if (child != null) {
+                treeTikZ.append("child { ");
+                treeTikZ.append(child.toTikZ());
+                treeTikZ.append(" }\n");
+            }
+        }
+        return treeTikZ.toString();
+    }
+
+    /**
+     * Writes the tree as a TikZ picture. A TikZ picture embeds TikZ code so that
+     * LaTeX undertands it.
+     */
+    public String toTikZPicture() {
+        return "\\begin{tikzpicture}[tree layout]\n\\" + toTikZ() + ";\n\\end{tikzpicture}";
+    }
+
+    /**
+     * Writes the tree as a LaTeX document which can be compiled (using the LuaLaTeX
+     * engine). Be careful that such code will not compile with PDFLaTeX, since the
+     * tree drawing algorithm is written in Lua. The code is not very readable as
+     * such, but you can have a look at the outputted file if you want to understand
+     * better. <br>
+     * <br>
+     * The result can be used with the command:
+     *
+     * <pre>
+     * lualatex some-file.tex
+     * </pre>
+     */
+    public String toLaTeXLua() {
+        return "\\RequirePackage{luatex85}\n\\documentclass{standalone}\n\n\\usepackage[T1]{fontenc}\n\\usepackage{tikz}\n\n\\usetikzlibrary{graphdrawing, graphdrawing.trees}\n\n\\begin{document}\n\n"
+                + toTikZPicture() + "\n\n\\end{document}\n%% Local Variables:\n%% TeX-engine: luatex\n%% End:";
+    }
+
+    /**
+     * Writes the tree as a forest picture. Returns the tree in forest enviroment
+     * using the latex code of the tree
+     */
+    public String toForestPicture() {
+        return "\\begin{forest}for tree={rectangle,draw, l sep=20pt}" + toLaTexTree() + ";\n\\end{forest}";
+    }
+
+    /**
+     * Writes the tree as a LaTeX document which can be compiled using PDFLaTeX.
+     * <br>
+     * <br>
+     * The result can be used with the command:
+     *
+     * <pre>
+     * pdflatex some-file.tex
+     * </pre>
+     */
+    public String toLaTeX() {
+        return "\\documentclass[border=5pt]{standalone}\n\n\\usepackage[T1]{fontenc}\n\\usepackage{tikz}\n\\usepackage{forest}\n\n\\begin{document}\n\n"
+                + toForestPicture() + "\n\n\\end{document}\n%% Local Variables:\n%% TeX-engine: pdflatex\n%% End:";
     }
 
     static public AbstractSyntaxTree toAST(ParseTree parseTree) {
